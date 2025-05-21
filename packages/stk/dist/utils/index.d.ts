2b599f6b12ab904cf1c9bd52708cb053e085efe4
1yasa <xiewendao@gmail.com>
2025-05-21 19:28:08 +0800

    fix: remove smoothScroll.js to prevent nextjs error

--- packages/stk/dist/utils/index.d.ts
+++ packages/stk/dist/utils/index.d.ts
@@ -1,6 +1,4 @@
 export { default as $ } from './$';
 export { default as StickyTableHeader } from './StickyTableHeader';
-export { default as SmoothScroll } from './SmoothScroll';
-export type { ArgsSmoothScroll } from './SmoothScroll.types';
 export * from './is';
 export * from './date';
