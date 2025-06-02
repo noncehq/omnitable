b7697d148b01c30ea81687ee7c21f121b0db7c50
aquariuslt <superaquariuslt@gmail.com>
2025-06-02 14:44:24 +0800

    refactor: improve documentation and fix typos in code comments

--- packages/examples/pages/layout.tsx
+++ packages/examples/pages/layout.tsx
@@ -1,20 +1,20 @@
 import { useMemo } from 'react'
 import { App } from 'antd'
-import en_US from 'antd/locale/en_US'
 import { useOutlet } from 'react-router-dom'
 
-import { AntdConfigProvider } from '@@omnitable/omnitable/appframe/components'
-import { getAntdTheme } from '@@omnitable/omnitable/appframe/theme'
+import { AntdConfigProvider } from '@omnitable/appframe/components'
+import { getAntdTheme } from '@omnitable/appframe/theme'
+
+import Nav from './Nav'
 
 import styles from './layout.module.css'
-import Nav from './Nav'
 
 export default () => {
   const outlet = useOutlet()
   const theme = useMemo(() => getAntdTheme('light'), [])
 
   return (
-    <AntdConfigProvider locale={en_US} theme={theme}>
+    <AntdConfigProvider theme="light">
       <App prefixCls="omni">
         <div className={styles._local}>
           <Nav></Nav>
